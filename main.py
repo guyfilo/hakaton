10fab4c26b5ce755f388d728a05db5677ce20697
danweil24 <danweil24@gmail.com>
2021-06-02 20:40:55 +0300

    hnhj

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def print_hi(name):
     print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
 
 kierhewir ewlir
+pass
+pass
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
