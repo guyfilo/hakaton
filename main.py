4816444455270618ee53605ad365357a1cf51e22
danweil24 <danweil24@gmail.com>
2021-06-02 20:37:49 +0300

    hnhj

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
 
-
+kierhewir ewlir
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
