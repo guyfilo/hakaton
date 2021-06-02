19ed0a41a9026731f4921b094dfda195edc79408
danweil24 <danweil24@gmail.com>
2021-06-03 01:11:00 +0300

    pre pro

--- location_filter.py
+++ location_filter.py
@@ -3,27 +3,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from _datetime import datetime
 from sklearn.neighbors import KNeighborsClassifier
-training_set_locations =[]
-values = {"BATTERY":0, "THEFT":1, "CRIMINAL DAMAGE":2 ,"DECEPTIVE PRACTICE":3 , "ASSAULT":4}
+
+training_set_locations = []
+values = {"BATTERY": 0, "THEFT": 1, "CRIMINAL DAMAGE": 2, "DECEPTIVE PRACTICE": 3, "ASSAULT": 4}
+
 def change_data(data_frame):
     data_frame.dropna(inplace=True)
     response_vector = data_frame['Primary Type'].apply(lambda x: values[x]).to_numpy()
     loaction_x = data_frame['X Coordinate'].to_numpy()
     loaction_y = data_frame['Y Coordinate'].to_numpy()
     locations = np.append(np.array([loaction_y]).T, np.array([loaction_x]).T, 1)
-    k_neighbors = KNeighborsClassifier(n_neighbors=1)
+    k_neighbors = KNeighborsClassifier(n_neighbors=20)
     k_neighbors.fit(locations, response_vector)
-    return k_neighbors,locations,response_vector
+    return k_neighbors, locations, response_vector
 
 
-def create_list(x, y):
-    x = change_data(training).predict_proba([[x, y]])
-    print(x)
+def create_list(data, training):
+    x = change_data(training).predict_proba(data)
+    data['Propability_location_based_B'] = x[:, 0]
+    data['Propability_location_based_T'] = x[:, 1]
+    data['Propability_location_based_C'] = x[:, 2]
+    data['Propability_location_based_D'] = x[:, 3]
+    data['Propability_location_based_A'] = x[:, 4]
+    return data
 
 
 if __name__ == '__main__':
-    training = pd.read_csv("training set")
-    a,b,c = change_data(training)
-    pred = a.predict_proba(b)
-
-
+    k, l, r = change_data(training)
+    print(k.score(l, r))
--- main.py
+++ main.py
@@ -7,16 +7,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from _datetime import datetime
-training_set_locations =[]
+import location_filter
 
-def pre_proccing(name):
-    # Use a breakpoint in the code line below to debug your script.
-    training = pd.read_csv("test.csv")
-    data['date'] = pd.to_datetime(data['date'], errors='coerce')
-    data['date'] = ((data['date'] - datetime(1970, 1, 1)).dt.total_seconds()) / (24 * 60 * 60)
+training_set_locations = []
+values = {"BATTERY": 0, "THEFT": 1, "CRIMINAL DAMAGE": 2, "DECEPTIVE PRACTICE": 3, "ASSAULT": 4}
+
+
+def pre_proccing():
+    df = pd.read_csv("training set")
+    df.dropna(inplace=True)
     df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
     df['Hour'] = df['Date'].dt.hour
-
+    df = location_filter.create_list(df, df)
+    df['Arrest'] = df['Arrest'].astype(int)
+    df['Domestic'] = df['Domestic'].astype(int)
+    df = pd.get_dummies(df, columns=['Block'], drop_first=True)
+    df.drop(columns=['id', 'price', 'long', 'lat'], inplace=True)
+    response_vector = df['Primary Type'].apply(lambda x: values[x]).to_numpy()
+    training_point = df.to_numpy()
+    return training_point, response_vector
 
 
 # Press the green button in the gutter to run the script.
@@ -24,4 +33,4 @@ if __name__ == '__main__':
     pre_proccing("a")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-#hosafti comment
+# hosafti comment
