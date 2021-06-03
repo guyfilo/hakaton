6466d56a8e9c96867def49c0dd848bea5469919d
danweil24 <danweil24@gmail.com>
2021-06-03 19:07:46 +0300

    pre pro

--- preprocess.py
+++ preprocess.py
@@ -14,7 +14,7 @@ values = {"BATTERY": 0, "THEFT": 1, "CRIMINAL DAMAGE": 2, "DECEPTIVE PRACTICE": 
 " features, column_title, feature_label='Primary Type', k=20"
 # ,(['X Coordinate', 'Y Coordinate'], 'block', 'Block',1)(['X Coordinate', 'Y Coordinate'], 'beat', 'Beat',1)
 prod_feacture_args = [(['X Coordinate', 'Y Coordinate'], 'location', 'Primary Type', 100),
-                      (['DayTime'], 'dayTime', 'Primary Type', 100)]
+                      (['DayTime'], 'dayTime', 'Primary Type', 100),(['X Coordinate', 'Y Coordinate'], 'beat', 'Beat',4)]
 
 
 
@@ -36,7 +36,7 @@ class PreProcessing:
         df['Arrest'] = df['Arrest'].astype(int)
         df['Domestic'] = df['Domestic'].astype(int)
         df = pd.get_dummies(df, columns=[ 'WeekDay', 'Month', 'MonthDay'], drop_first=True)
-        df.drop(columns=['Date', 'IUCR', 'FBI Code', 'Description', 'Case Number', 'Location Description', 'Updated On',
+        df.drop(columns=['Date', 'Block', 'IUCR', 'FBI Code', 'Description', 'Case Number', 'Location Description', 'Updated On',
                          'Location'], inplace=True)
 
         return df
@@ -50,7 +50,14 @@ class PreProcessing:
         for prob in self.prob_feature:
             prob.add_features(data)
         response_vector = data['Primary Type'].apply(lambda x: values[x])
-        data.drop(columns=['Block', 'X Coordinate', 'Y Coordinate', 'DayTime', 'Beat', 'Primary Type'], inplace=True)
+        data.drop(columns=['X Coordinate', 'Y Coordinate', 'DayTime', 'Beat', 'Primary Type'], inplace=True)
+        if not(is_training):
+            missing_cols =  set(self.training_data.columns)-set(data.columns)
+            # Add a missing column in test set with default value equal to 0
+            for c in missing_cols:
+                data[c] = 0
+            # Ensure the order of column in the test set is in the same order than in train set
+            data = data[self.training_data.columns]
         return data, response_vector
 
 
@@ -70,13 +77,13 @@ if __name__ == '__main__':
     print("tree")
     print(tree.score(X, y))
     print(tree.score(Xv, yv))
-    rand_forest = en.RandomForestClassifier(n_estimators=200)
+    rand_forest = en.RandomForestClassifier(n_estimators=200,max_depth=10)
     rand_forest.fit(X,y)
     print("rnd forest")
     print(rand_forest.score(X, y))
     print(rand_forest.score(Xv, yv))
 
-שטויות בסוף
+
 
 
 
