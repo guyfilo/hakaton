356a1209c08fce24b7b07c27c7c02569d40162b4
guyfilo <guyfilo94@gmail.com>
2021-06-03 15:29:32 +0300

    testing

--- preprocess.py
+++ preprocess.py
@@ -30,7 +30,7 @@ values = {"BATTERY": 0, "THEFT": 1, "CRIMINAL DAMAGE": 2, "DECEPTIVE PRACTICE": 
 " features, column_title, feature_label='Primary Type', k=20"
 #(['X Coordinate', 'Y Coordinate'], 'block', 'Block'),(['X Coordinate', 'Y Coordinate'], 'beat', 'Beat')
 prod_feacture_args = [(['X Coordinate', 'Y Coordinate'], 'location', 'Primary Type', 1),
-                      (['DayTime'], 'dayTime', 'Primary Type', 100) ]
+                      (['DayTime'], 'dayTime', 'Primary Type', 15) ]
 
 
 
@@ -66,7 +66,6 @@ class PreProcessing:
             data = self.basic_preprocessing(data)
         for prob in self.prob_feature:
             prob.add_features(data)
-            print("msamdk")
         response_vector = data['Primary Type'].apply(lambda x: values[x])
         data.drop(columns=['Block', 'X Coordinate', 'Y Coordinate', 'DayTime', 'Beat', 'Primary Type'], inplace=True)
         return data, response_vector
@@ -76,11 +75,9 @@ if __name__ == '__main__':
     train = pd.read_csv('training set')
     data_pro = PreProcessing(train)
     X, y = data_pro.load_new_features(data_pro.training_data, True)
-    print("finish training")
-    val = pd.read_csv('validation set')
+    val = pd.read_csv('test set')
     Xv, yv = data_pro.load_new_features(val, False)
-
-    tree = tr.DecisionTreeClassifier(max_depth=9)
+    tree = tr.DecisionTreeClassifier(max_depth=10)
     tree.fit(X, y)
     print(tree.score(X, y))
     print(tree.score(Xv, yv))
