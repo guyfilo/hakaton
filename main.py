b944ca8c48a5911d722bc1bd1cf0116caa943e1d
danweil24 <danweil24@gmail.com>
2021-06-03 17:34:05 +0300

    pre pro

--- main.py
+++ main.py
@@ -1,51 +1,38 @@
 # coding=utf-8
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-import numpy as np
 import pandas as pd
-import re
-import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
-from _datetime import datetime
-import location_filter
-from sklearn.tree import DecisionTreeClassifier
+import sklearn.model_selection
+import sklearn.tree as tr
+import  sklearn.ensemble as en
+from preprocess import *
+from sklearn.ensemble import BaggingClassifier
+from location_filter import *
 
 training_set_locations = []
 values = {"BATTERY": 0, "THEFT": 1, "CRIMINAL DAMAGE": 2, "DECEPTIVE PRACTICE": 3, "ASSAULT": 4}
-
-
-def pre_proccing():
-    df = pd.read_csv("training set")
-    df.dropna(inplace=True)
-    df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
-    df['Weekday'] = df['Date'].dt.dayofweek
-    df['Date'] = df['Date'].dt.hour + df['Date'].dt.minute / 60
-    df = location_filter.create_list(df, df)
-    df['Arrest'] = df['Arrest'].astype(int)
-    df['Domestic'] = df['Domestic'].astype(int)
-    #df = pd.get_dummies(df, columns=['Block'], drop_first=True)
-    df.drop(columns=['IUCR', 'FBI Code', 'Description', 'Case Number', 'Year', 'Location Description', 'Updated On', 'Location', 'Block'], inplace=True)
-
-    """cor = df.corr()
-    sns.heatmap(cor,
-                xticklabels=cor.columns,
-                yticklabels=cor.columns)
-    plt.show()"""
-
-    response_vector = df['Primary Type'].apply(lambda x: values[x]).to_numpy()
-    df.drop(columns=['Primary Type'], inplace=True)
-    #training_point = df.to_numpy()
-    return df, response_vector
+" features, column_title, feature_label='Primary Type', k=20"
+#(['X Coordinate', 'Y Coordinate'], 'block', 'Block'),(['X Coordinate', 'Y Coordinate'], 'beat', 'Beat')
+prod_feacture_args = [(['X Coordinate', 'Y Coordinate'], 'location', 'Primary Type', 1),
+                      (['DayTime'], 'dayTime', 'Primary Type', 15) ]
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    X, y = pre_proccing()
-    """tree = DecisionTreeClassifier(max_depth=5)
-    scores = tree.fit(X, y).score(X, y)
-    print(scores)"""
+    train = pd.read_csv('training set')
+    data_pro = PreProcessing(train)
+    X, y = data_pro.load_new_features(data_pro.training_data, True)
+    val = pd.read_csv('test set')
+    Xv, yv = data_pro.load_new_features(val, False)
+    tree = BaggingClassifier(tr.DecisionTreeClassifier(max_depth=20), n_estimators=66)
+    tree.fit(X, y)
+    print(tree.score(X, y))
+    print(tree.score(Xv, yv))
+    rand_forest = en.RandomForestClassifier(n_estimators=200)
+    rand_forest.fit(X, y)
+    print("rnd forest")
+    print(rand_forest.score(X, y))
+    print(rand_forest.score(Xv, yv))
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
