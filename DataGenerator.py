a9f13a2bd61968be6f3a87c13184cc23ba7d5a9f
guyfilo <guyfilo94@gmail.com>
2021-06-03 16:08:35 +0300

    split new data

---
+++ DataGenerator.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("dataset_crimes.csv")
+df_2 = pd.read_csv("crimes_dataset_part2.csv")
+df = pd.concat([df, df_2])
+
+
+train = df.sample(frac=0.7, random_state=200)
+test_valid = df.drop(train.index)
+validation = test_valid.sample(frac=0.5, random_state=200)
+test = test_valid.drop(validation.index)
+
+pd.DataFrame.to_csv(train, "training set")
+pd.DataFrame.to_csv(validation, "validation set")
+pd.DataFrame.to_csv(test, "test set")
+
+
+
+
+"""
+v = pd.unique(df['Primary Type'])
+u = pd.unique(df['Location Description'])
+
+primary_types = {j: v[j] for j in range(5)}
+
+beat, location = df['Beat'], df['Location']
+
+beat_to_location = {beat[j]: location[j] for j in range(len(beat))}
+"""
+
+"""train = df.sample(frac=0.8, random_state=200)
+test_validation = df.drop(train.index)
+validation = df.sample(frac=0.5, random_state=200)
+test = df.drop(validation.index)
+
+pd.DataFrame.to_csv(train, "training set")
+pd.DataFrame.to_csv(validation, "validation set")
+pd.DataFrame.to_csv(test, "test set")"""
--- preprocess.py
+++ preprocess.py
@@ -3,34 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.model_selection
 import sklearn.tree as tr
-
-df = pd.read_csv("dataset_crimes.csv")
-v = pd.unique(df['Primary Type'])
-u = pd.unique(df['Location Description'])
-
-primary_types = {j: v[j] for j in range(5)}
-
-beat, location = df['Beat'], df['Location']
-
-beat_to_location = {beat[j]: location[j] for j in range(len(beat))}
-
-train = df.sample(frac=0.8, random_state=200)
-test_validation = df.drop(train.index)
-validation = df.sample(frac=0.5, random_state=200)
-test = df.drop(validation.index)
-
-pd.DataFrame.to_csv(train, "training set")
-pd.DataFrame.to_csv(validation, "validation set")
-pd.DataFrame.to_csv(test, "test set")
-
+import sklearn.ensemble as en
 from location_filter import *
 
 training_set_locations = []
 values = {"BATTERY": 0, "THEFT": 1, "CRIMINAL DAMAGE": 2, "DECEPTIVE PRACTICE": 3, "ASSAULT": 4}
 " features, column_title, feature_label='Primary Type', k=20"
 #(['X Coordinate', 'Y Coordinate'], 'block', 'Block'),(['X Coordinate', 'Y Coordinate'], 'beat', 'Beat')
-prod_feacture_args = [(['X Coordinate', 'Y Coordinate'], 'location', 'Primary Type', 1),
-                      (['DayTime'], 'dayTime', 'Primary Type', 15) ]
+prod_feacture_args = [(['X Coordinate', 'Y Coordinate'], 'location', 'Primary Type', 10),
+                      (['DayTime'], 'dayTime', 'Primary Type', 30) ]
 
 
 
@@ -75,12 +56,19 @@ if __name__ == '__main__':
     train = pd.read_csv('training set')
     data_pro = PreProcessing(train)
     X, y = data_pro.load_new_features(data_pro.training_data, True)
-    val = pd.read_csv('test set')
+    val = pd.read_csv('validation set')
     Xv, yv = data_pro.load_new_features(val, False)
     tree = tr.DecisionTreeClassifier(max_depth=10)
     tree.fit(X, y)
+    print("tree")
     print(tree.score(X, y))
     print(tree.score(Xv, yv))
+    rand_forest = en.RandomForestClassifier(n_estimators=200)
+    rand_forest.fit(X,y)
+    print("rnd forest")
+    print(rand_forest.score(X, y))
+    print(rand_forest.score(Xv, yv))
+
 
 
 
