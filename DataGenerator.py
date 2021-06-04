523af0a982d293e43fc59c701a7643d01ac744b2
guyfilo <guyfilo94@gmail.com>
2021-06-04 07:58:53 +0300

    halas

--- DataGenerator.py
+++ DataGenerator.py
@@ -4,6 +4,7 @@ import numpy as np
 df = pd.read_csv("dataset_crimes.csv", index_col=0)
 df_2 = pd.read_csv("crimes_dataset_part2.csv", index_col=0)
 df = pd.concat([df, df_2])
+df.to_csv("data")
 
 
 train = df.sample(frac=0.8, random_state=88)
